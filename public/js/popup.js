cba11477323f421b80de470f0435416d3f94f208
Daniel Mihai <danikmihai23@mail.ru>
2024-09-04 19:50:24 +0300

    Promotions block
    Add block with promotions

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,17 +1,47 @@
-// Выборка значений
-var popup_btn = document.getElementById("popup");
-var popup_content = document.getElementById("popup-content");
-var popup_close_btn = document.getElementById("popup-close");
-var popup_overlay = document.getElementById("overlay-popup");
+// // Выборка значений
+// var popup_btn = document.querySelectorAll(".popup--btn");
+// var popup_content = document.getElementById("popup-content");
+// var popup_close_btn = document.getElementById("popup-close");
+// var popup_overlay = document.getElementById("overlay-popup");
 
-// // Обработчик событий при клике (открытие)
-popup_btn.addEventListener("click", function() {
-    popup_content.classList.toggle("active");
-    popup_overlay.classList.add("active");
+// // // Обработчик событий при клике (открытие)
+// popup_btn.addEventListener("click", function() {
+//     popup_content.classList.toggle("active");
+//     popup_overlay.classList.add("active");
+// });
+
+// // Обработчик событий при клике (закрытие)
+// popup_close_btn.addEventListener("click", function() {
+//     popup_content.classList.toggle("active");
+//     popup_overlay.classList.remove("active");
+// });
+
+const overlay = document.querySelector('.overlay-popup');
+const popup_dropdown = document.getElementById('popup-content');
+const toggleButtons = document.querySelectorAll('.popup--btn');
+const btnClose = document.getElementById("popup-close");
+
+// Функция для открытия модального окна
+const openPopup = () => {
+    overlay.classList.add('active');
+    popup_dropdown.classList.add('active');
+};
+
+// Функция для закрытия модального окна
+const closePopup = () => {
+    overlay.classList.remove('active');
+    popup_dropdown.classList.remove('active');
+};
+
+// Добавляем обработчик события для всех кнопок
+toggleButtons.forEach(button => {
+    button.addEventListener('click', openPopup);
 });
 
-// Обработчик событий при клике (закрытие)
-popup_close_btn.addEventListener("click", function() {
-    popup_content.classList.toggle("active");
-    popup_overlay.classList.remove("active");
-});
+// Добавляем обработчик события для кнопки закрытия
+btnClose.addEventListener('click', closePopup);
+
+// Также можно закрывать модальное окно при клике вне его
+overlay.addEventListener('click', closePopup);
+;
+
