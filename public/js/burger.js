8ba55e6b57b3c32961e2fc546fb7b98a924def5b
Daniel Mihai <danikmihai23@mail.ru>
2024-09-05 10:46:48 +0300

    Direction block
    Add block with all directions in fitensslove center

--- public/js/burger.js
+++ public/js/burger.js
@@ -2,16 +2,16 @@
 var button_open_burger = document.getElementById("burger-icon");
 var button_close_burger = document.getElementById("close-icon");
 var dropdown_burger = document.getElementById("burger-dropdown");
-var overlay_burher = document.getElementById("overlay");
+var overlay_burger = document.getElementById("overlay");
 
 // Обработчик событий при клике (открытие)
 button_open_burger.addEventListener("click", function() {
     dropdown_burger.classList.toggle("active");
-    overlay.classList.add("active");
+    overlay_burger.classList.add("active");
 });
 
 // Обработчик событий при клике (закрытие)
 button_close_burger.addEventListener("click", function() {
     dropdown_burger.classList.toggle("active");
-    overlay.classList.remove("active");
+    overlay_burger.classList.remove("active");
 });
